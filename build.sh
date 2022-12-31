711ef8d8649c16f8f1f62104e1058ea2e9bb0bce
bozer <gamerpig197@gmail.com>
2022-12-31 09:48:20 -0500

    update ARM architecture

--- build.sh
+++ build.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 if ! command -v go &> /dev/null || ! command -v upx &> /dev/null ; then
-    echo -e "\e[1;31m\"UPX\" packer or \"Go\" compiler has not installed!!!\e[0m"
+    echo -e "\e[1;31m\"UPX\" packer, or \"Go\" compiler has not been installed!!!\e[0m"
     exit
 fi
 
 clear
 
+echo -e "\n\e[1;32mBUILDING x32 DEFAULT...\e[0m\n"
 GOOS=linux GOARCH=386 go build -ldflags "-s -w" -o bin/$1_x32 cmd/main.go && upx -9 bin/$1_x32 && clear
+echo -e "\n\e[1;32mBUILDING x32 MIPS...\e[0m\n"
 GOOS=linux GOARCH=mips go build -ldflags "-s -w" -o bin/$1_mips_x32 cmd/main.go && upx -9 bin/$1_mips_x32 && clear
-
-# If you want to use the pure x64 executable just uncomment it, but IDK why UPX can't compress the MIPS x64 executable file.
-
-# GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o bin/$1_x64 cmd/main.go && upx -9 bin/$1_x64 && clear
-# GOOS=linux GOARCH=mips64 go build -ldflags "-s -w" -o bin/$1_mips_x64 cmd/main.go && upx -9 bin/$1_mips_x64 && clear
+echo -e "\n\e[1;32mBUILDING x32 ARM...\e[0m\n"
+GOOS=linux GOARCH=arm go build -ldflags "-s -w" -o bin/$1_arm_x32 cmd/main.go && upx -9 bin/$1_arm_x32 && clear
 
 echo ""
 echo -e "\e[1;32m               BUILD SUCCESS!!!                \e[0m"
